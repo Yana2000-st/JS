365e76bfc349986c8a264d10319559525df32a34
Yana2000-st <yana.kolodyaznaya2000@gmail.com>
2024-12-26 13:55:12 +0300

    commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let swiperInstance; // Переменная для хранения экземпляра Swiper
+let swiperInstance;
 
 function manageSwiper() {
-  const screenWidth = window.innerWidth; // Получаем ширину экрана
+  const screenWidth = window.innerWidth; 
 
-  if (screenWidth <= 767) { // Условие для активации свайпера (меньше или равно 768px)
-    if (!swiperInstance) { // Если Swiper ещё не инициализирован
+  if (screenWidth <= 767) {
+    if (!swiperInstance) { 
       swiperInstance = new Swiper('.swiper', {
         pagination: {
           el: '.swiper-pagination',
@@ -16,15 +16,15 @@ function manageSwiper() {
       });
     }
   } else {
-    // Удаляем Swiper, если он существует, на больших экранах
+  
     if (swiperInstance) {
-      swiperInstance.destroy(true, true); // Полностью уничтожаем Swiper
-      swiperInstance = null; // Очищаем переменную
+      swiperInstance.destroy(true, true); 
+      swiperInstance = null; 
     }
   }
 }
 
-// Вызываем функцию при загрузке страницы и изменении размера окна
+
 manageSwiper();
 window.addEventListener('resize', manageSwiper);
 document.getElementById("toggleButton").style.setProperty('--rotate', '45deg');
@@ -32,37 +32,37 @@ document.getElementById("toggleButton").style.setProperty('--rotate', '45deg');
 
 function readMore() {
   const toggleButton = document.getElementById("toggleButton");
-  const hiddenElements = document.querySelectorAll(".hidden"); // Скрытые элементы
-  const largeScreenHiddenElements = document.querySelectorAll(".large-screen-hidden"); // Элементы для скрытия на больших экранах
-  const screenWidth = window.innerWidth; // Текущая ширина экрана
+  const hiddenElements = document.querySelectorAll(".hidden"); 
+  const largeScreenHiddenElements = document.querySelectorAll(".large-screen-hidden"); 
+  const screenWidth = window.innerWidth; 
 
   if (screenWidth >= 1120) {
-      // Убираем 3 картинки при ширине 1120px
+     
       if (largeScreenHiddenElements[0].style.display === "" || largeScreenHiddenElements[0].style.display === "none") {
           largeScreenHiddenElements.forEach((el) => {
-              el.style.display = "grid"; // Показываем элементы
+              el.style.display = "grid"; 
           });
           toggleButton.innerHTML = "Скрыть";
           toggleButton.style.setProperty('--rotate', '225deg')
       } else {
           largeScreenHiddenElements.forEach((el) => {
-              el.style.display = "none"; // Скрываем 3 картинки
+              el.style.display = "none"; 
           });
           toggleButton.innerHTML = "Показать все";
           toggleButton.style.setProperty('--rotate', '45deg')
       }
   } else {
-      // Управление скрытыми элементами на других разрешениях
+
 
       if (hiddenElements[0].style.display === "" ||  hiddenElements[0].style.display === "none") {
           hiddenElements.forEach((el) => {
-              el.style.display = "grid"; // Показываем элементы
+              el.style.display = "grid"; 
           });
           toggleButton.innerHTML = "Скрыть";
           toggleButton.style.setProperty('--rotate', '225deg')
       } else {
           hiddenElements.forEach((el) => {
-              el.style.display = "none"; // Скрываем элементы
+              el.style.display = "none"; 
           });
           toggleButton.innerHTML = "Показать все";
           toggleButton.style.setProperty('--rotate', '45deg')
